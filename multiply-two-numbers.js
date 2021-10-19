668b6fb0252c1fc806bcb1fcf698b6e1b62849e2
Zenater <zent230096@mail.ru>
2021-10-19 17:40:42 +0300

    new kata

---
+++ multiply-two-numbers.js
@@ -0,0 +1,11 @@
+
+/*      
+
+Function 3 - multiplying two numbers
+Implement a function which multiplies two numbers.
+   */
+
+function multiply(a,b){
+    var result = a*b;
+         return result;
+     }
